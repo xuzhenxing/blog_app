38c531ab5b5b208e30f303210fe99ad5077dfb0f
xuzhenxing <c804510737@qq.com>
2015-01-16 09:31:27 +0800

    Change time locale,language change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917091849) do
+ActiveRecord::Schema.define(version: 20150109101925) do
 
   create_table "blogs", force: true do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140917091849) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           default: false
+    t.datetime "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
